068d672f6b08b1d76389848f4fcebbba04ed4f99
Artjoms <developer.fomenko.artjoms@gmail.com>
2021-07-14 15:49:46 +0300

    Fixes
    Fixes

--- www/defaultMailPlugin.js
+++ www/defaultMailPlugin.js
@@ -1,8 +1,33 @@
-function DefaultMailPlugin() {}
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
 
-DefaultMailPlugin.prototype.open = function() {
-  var options = {};
-  cordova.exec(successCallback, errorCallback, 'DefaultMailPlugin', 'open', [options]);
+var argscheck = require('cordova/argscheck');
+var exec = require('cordova/exec');
+
+function DefaultMailPlugin() {
 }
+
+DefaultMailPlugin.prototype.open = function (successCallback, errorCallback) {
+  argscheck.checkArgs('fF', 'DefaultMailPlugin.open', arguments);
+  exec(successCallback, errorCallback, 'DefaultMailPlugin', 'open', []);
+};
 
 module.exports = new DefaultMailPlugin();
